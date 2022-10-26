6b0c9d7f2dfa4e36dd04fa67b7a805d42afa43cf
Martin Agusti <70919001+martinagusti@users.noreply.github.com>
2022-10-26 13:45:09 +0200

    Add files via upload

--- puntaje.js
+++ puntaje.js
@@ -8,12 +8,17 @@ var tiebrake = 0
 var tiebrake2 = 0
 
 
+
 var lblAgregar = document.getElementById("lbl_agregar")
 var lblAgregar2 = document.getElementById("lbl_agregar2")
 var lblGames = document.getElementById("games")
 var lblGames2 = document.getElementById("games2")
 var lblSets = document.getElementById("sets")
 var lblSets2 = document.getElementById("sets2")
+var set1 = document.getElementById("set1")
+var set2 = document.getElementById("set2")
+var set3 = document.getElementById("set3")
+
 
 
 function agregar(){
@@ -21,8 +26,19 @@ function agregar(){
    if(games == 6 & games2 == 6){
       tiebrake ++
       lblAgregar.innerText = tiebrake
-      if(tiebrake == 7){
+      if((tiebrake == 7 & tiebrake2 < 6) || (tiebrake > 7 & tiebrake > (tiebrake2 + 1))){
+         games ++
          sets ++
+         if(sets == 1){
+            set1.innerText = set1.innerText + " " + games + " - " + games2
+         }
+         if(sets == 2){
+            set2.innerText = set2.innerText  + " "+ games + " - " + games2
+         }
+         if(sets == 3){
+            set3.innerText = set3.innerText  + " "+ games + " - " + games2
+         }
+
          lblSets.innerText = sets
          games = 0
          games2 = 0
@@ -30,6 +46,11 @@ function agregar(){
          lblGames2.innerText = 0
          lblAgregar.innerText = 0
          lblAgregar2.innerText = 0
+         tiebrake = 0
+         tiebrake2 = 0
+        
+
+
       }
 
    }else{
@@ -60,18 +81,42 @@ function agregar(){
       }
       if(games == 6 & games2 < 5){
         sets ++
+
+        if(sets == 1){
+         set1.innerText = set1.innerText  + " "+ games + " - " + games2
+      }
+      if(sets == 2){
+         set2.innerText = set2.innerText  + " "+ games + " - " + games2
+      }
+      if(sets == 3){
+         set3.innerText = set3.innerText  + " "+ games + " - " + games2
+      }
+
         lblSets.innerText = sets
         games = 0
         lblGames.innerText = 0
+        
      
       }
       if(games == 7 & games2 == 5){
          sets ++
+
+         if(sets == 1){
+            set1.innerText = set1.innerText + " " + games + " - " + games2
+         }
+         if(sets == 2){
+            set2.innerText = set2.innerText  + " "+ games + " - " + games2
+         }
+         if(sets == 3){
+            set3.innerText = set3.innerText  + " "+ games + " - " + games2
+         }
+
          lblSets.innerText = sets
          games = 0
          games2 = 0
          lblGames.innerText = 0
          lblGames2.innerText = 0
+        
       
        }
 
@@ -118,8 +163,20 @@ function restar(){
       if(games == 6 & games2 == 6){
          tiebrake2 ++
          lblAgregar2.innerText = tiebrake2
-         if(tiebrake2 == 7){
+         if((tiebrake2 == 7 & tiebrake < 6) || (tiebrake2 > 7 & tiebrake2 > (tiebrake + 1))){
+            games2 ++
             sets2 ++
+
+            if(sets2 == 1){
+               set1.innerText = set1.innerText + " " + games + " - " + games2
+            }
+            if(sets2 == 2){
+               set2.innerText = set2.innerText  + " "+ games + " - " + games2
+            }
+            if(sets2 == 3){
+               set3.innerText = set3.innerText  + " "+ games + " - " + games2
+            }
+
             lblSets2.innerText = sets2
             games = 0
             games2 = 0
@@ -127,6 +184,8 @@ function restar(){
             lblGames2.innerText = 0
             lblAgregar.innerText = 0
             lblAgregar2.innerText = 0
+            tiebrake = 0
+            tiebrake2 = 0
          }
    
       }else{
@@ -157,6 +216,17 @@ function restar(){
     }
     if(games2 == 6 & games < 5){
       sets2 ++
+
+      if(sets2 == 1){
+         set1.innerText = set1.innerText + " " + games + " - " + games2
+      }
+      if(sets2 == 2){
+         set2.innerText = set2.innerText  + " "+ games + " - " + games2
+      }
+      if(sets2 == 3){
+         set3.innerText = set3.innerText  + " "+ games + " - " + games2
+      }
+
       lblSets2.innerText = sets2
       games2 = 0
       lblGames2.innerText = 0
@@ -164,6 +234,17 @@ function restar(){
     }
     if(games2 == 7 & games == 5){
       sets2 ++
+      
+      if(sets2 == 1){
+         set1.innerText = set1.innerText + " " + games + " - " + games2
+      }
+      if(sets2 == 2){
+         set2.innerText = set2.innerText  + " "+ games + " - " + games2
+      }
+      if(sets2 == 3){
+         set3.innerText = set3.innerText  + " "+ games + " - " + games2
+      }
+
       lblSets2.innerText = sets2
       games2 = 0
       games = 0
